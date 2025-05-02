3b9d9ce28b91b7d5ff3007846eb93ef4b6091b35
quyetnd1711 <nquyet76@gmail.com>
2025-05-02 21:46:10 +0700

    fix: reset job if change time schedule

--- src/main/java/vn/eren/authenticationstructuresp/config/scheduler/QuartzConfig.java
+++ src/main/java/vn/eren/authenticationstructuresp/config/scheduler/QuartzConfig.java
@@ -8,10 +8,13 @@ import vn.eren.authenticationstructuresp.config.scheduler.job.RemoveTokenExpired
 @Configuration
 public class QuartzConfig {
 
+    private static final String REMOVE_TOKEN_JOB_IDENTITY = "removeTokenExpired";
+    private static final String CRON_EXPRESSION_REMOVE_TOKEN = "0 0 0/12 1/1 * ? *"; // Run once every half day
+
     @Bean
     public JobDetail removeTokenExpiredJobDetail() {
         return JobBuilder.newJob(RemoveTokenExpired.class)
-                .withIdentity("removeTokenExpired")
+                .withIdentity(REMOVE_TOKEN_JOB_IDENTITY)
                 .storeDurably()
                 .build();
     }
@@ -20,8 +23,8 @@ public class QuartzConfig {
     public Trigger removeTokenExpiredJobTrigger() {
         return TriggerBuilder.newTrigger()
                 .forJob(removeTokenExpiredJobDetail())
-                .withIdentity("removeTokenExpired")
-                .withSchedule(CronScheduleBuilder.cronSchedule("0 0 0/12 1/1 * ? *")) // Run once every half day
+                .withIdentity(REMOVE_TOKEN_JOB_IDENTITY + "Trigger")
+                .withSchedule(CronScheduleBuilder.cronSchedule(CRON_EXPRESSION_REMOVE_TOKEN)) // Run once every half day
                 .build();
     }
 
